d2b74e746b5ea4935f550d797b0b130639866184
Ivan Skorodumov <53996471+Selevn@users.noreply.github.com>
2020-08-10 23:59:51 +0300

    Bootstrap Navbar ui

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {BrowserRouter} from "react-router-dom";
 import {getConcerts} from "./js/db_imitate";
 import React from "react";
 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const wrapper = document.getElementById("root");
 wrapper ? ReactDOM.render(<BrowserRouter><App getConcerts={getConcerts}/></BrowserRouter>, wrapper) : false;

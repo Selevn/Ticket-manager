46531a3a08df841188ceb4b4e0defcbf00590144
Ivan Skorodumov <53996471+Selevn@users.noreply.github.com>
2020-08-12 16:14:54 +0300

    padding code fix

--- src/js/components/Main/MainComponents/Navbar/Navbar.jsx
+++ src/js/components/Main/MainComponents/Navbar/Navbar.jsx
@@ -4,14 +4,14 @@ import {NavLink} from "react-router-dom";
 import style from './Navbar.module.css'
 
 function Navbar(props) {
-    return(<div>
-        <NavLink to="/admin">admin</NavLink><br/>
-        <NavLink to="/login">Login</NavLink><br/>
-        <NavLink to="/home" activeClassName={style.active}>home</NavLink><br/>
-        <NavLink to="/search" activeClassName={style.active}>search</NavLink><br/>
-        <NavLink to="/schedule" activeClassName={style.active}>schedule</NavLink><br/>
-        <NavLink to="/contacts" activeClassName={style.active}>contacts</NavLink><br/>
-    </div>)
+  return (<div>
+    <NavLink to="/admin">admin</NavLink><br/>
+    <NavLink to="/login">Login</NavLink><br/>
+    <NavLink to="/home" activeClassName={style.active}>home</NavLink><br/>
+    <NavLink to="/search" activeClassName={style.active}>search</NavLink><br/>
+    <NavLink to="/schedule" activeClassName={style.active}>schedule</NavLink><br/>
+    <NavLink to="/contacts" activeClassName={style.active}>contacts</NavLink><br/>
+  </div>)
 }
 
 export default Navbar;

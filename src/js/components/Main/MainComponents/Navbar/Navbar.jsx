74423b08bc144f479737f8da4f95779bec2d3c47
Ivan Skorodumov <53996471+Selevn@users.noreply.github.com>
2020-08-12 16:55:58 +0300

    Naming fix

--- src/js/components/Main/MainComponents/Navbar/Navbar.jsx
+++ src/js/components/Main/MainComponents/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import style from './Navbar.module.css'
 import "./Navbar.css"
 
 
-function Navbar(props) {
+function AppNavbar(props) {
   return (
       <Navbar bg="light" expand="lg" className={`justify-content-center /*${style.container}*/`}>
         <div style={{textAlign: "center"}}>
@@ -14,19 +14,19 @@ function Navbar(props) {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav variant="pills" defaultActiveKey={useLocation().pathname}>
               <Nav.Item>
-                <Nav.Link eventKey="/home"><NavLink className={"s11"} to="/home">Home</NavLink></Nav.Link>
+                <Nav.Link eventKey="/home"><NavLink to="/home">Home</NavLink></Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link eventKey="/search"><NavLink className={"s11"} to="/search">Search</NavLink></Nav.Link>
+                <Nav.Link eventKey="/search"><NavLink to="/search">Search</NavLink></Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link eventKey="/schedule"><NavLink className={"s11"} to="/schedule">Schedule</NavLink></Nav.Link>
+                <Nav.Link eventKey="/schedule"><NavLink to="/schedule">Schedule</NavLink></Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link eventKey="/contacts"><NavLink className={"s11"} to="/contacts">Contacs</NavLink></Nav.Link>
+                <Nav.Link eventKey="/contacts"><NavLink to="/contacts">Contacs</NavLink></Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link eventKey="/login"><NavLink className={"s11"} to="/login">Login</NavLink></Nav.Link>
+                <Nav.Link eventKey="/login"><NavLink to="/login">Login</NavLink></Nav.Link>
               </Nav.Item>
             </Nav>
           </Navbar.Collapse>
@@ -35,4 +35,4 @@ function Navbar(props) {
   )
 }
 
-export default Navbar;
+export default AppNavbar;

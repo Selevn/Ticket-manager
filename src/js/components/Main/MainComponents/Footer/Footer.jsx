1e6cbe6b345e0281a76b7acf3f2078bd4725ec0d
Ivan Skorodumov <53996471+Selevn@users.noreply.github.com>
2020-08-14 19:29:27 +0300

    lang button update

--- src/js/components/Main/MainComponents/Footer/Footer.jsx
+++ src/js/components/Main/MainComponents/Footer/Footer.jsx
@@ -9,26 +9,26 @@ import {LanguageContext} from "../../../Contexts/LanguageContext.js"
 const Footer = () => (
     <LanguageContext.Consumer>
       {langProps => (
-            <footer className={style.footer}>
-              <Container id={style.containerBlock}>
-                <Row>
-                  <Col sm={0} xs={0} md={0} lg={3} xl={3}/>
-                  <Col sm={12} xs={12} md={12} lg={6} xl={6} className={style.col2}>
-                    Copyright (c) Ivan Skorodumov
-                    <br/>
-                    Minsk,2020
-                    <br/>
-                    All rights Reserved
-                  </Col>
-                  <Col sm={12} xs={12} md={12} lg={3} xl={3} style={{textAlign: "right"}} className={style.col3}>
-                    <button onClick={langProps.toggleLanguage} className={style.langButton}>
-                      {languageSrc.lang[langProps.language]}
-                    </button>
-                  </Col>
-                </Row>
-              </Container>
-            </footer>
-        )
+          <footer className={style.footer}>
+            <Container id={style.containerBlock}>
+              <Row>
+                <Col sm={0} xs={0} md={0} lg={3} xl={3}/>
+                <Col sm={12} xs={12} md={12} lg={6} xl={6} className={style.col2}>
+                  Copyright (c) Ivan Skorodumov
+                  <br/>
+                  Minsk,2020
+                  <br/>
+                  All rights Reserved
+                </Col>
+                <Col sm={12} xs={12} md={12} lg={3} xl={3} style={{textAlign: "right"}} className={style.col3}>
+                  <button onClick={langProps.toggleLanguage} className={style.langButton}>
+                    {languageSrc.lang[langProps.language]}
+                  </button>
+                </Col>
+              </Row>
+            </Container>
+          </footer>
+      )
       }
     </LanguageContext.Consumer>
 )

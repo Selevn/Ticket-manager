5f9084424916237f03435e0cec4eab78b1958324
Ivan Skorodumov <53996471+Selevn@users.noreply.github.com>
2020-08-19 15:32:55 +0300

    layout

--- src/js/components/Main/Search/Search.jsx
+++ src/js/components/Main/Search/Search.jsx
@@ -1,7 +1,53 @@
 import React from 'react'
 
-const Search = () => (<div>
-  <h3>SearchPage</h3>
-</div>)
+import {LanguageContext} from "../../Contexts/LanguageContext.js"
+import languagePack from "../../../language.js";
+import {Col, Container, Row} from "react-bootstrap";
+
+import "./Search.css"
+import style from "./Search.module.css"
+
+const Search = () => (<>
+  <LanguageContext.Consumer>
+    {langProps => (
+        <Container>
+          <Row>
+            <Col xs={12} sm={12} md={3} lg={2} xl={3}/>
+            <Col className={style.headerCol}><h3>{languagePack.search[langProps.language]}</h3></Col>
+            <Col xs={12} sm={12} md={3} lg={2} xl={3}/>
+          </Row>
+          <Row>
+            <Col xs={12} sm={12} md={3} lg={2} xl={3}/>
+            <Col className={style.searchCol}>
+              <form>
+                <Container>
+                  <Row>
+                    <Col className={style.center}>
+                      <input placeholder={languagePack.place[langProps.language]} type={"text"}/>
+                      <br/>
+                      <input placeholder={languagePack.band[langProps.language]} type={"text"}/>
+                      <br/>
+                    </Col>
+                    <Col className={style.center}>
+                      <input type={"date"}/>
+                      <br/>
+                      <input placeholder={languagePack.ticketCounter[langProps.language]} type={"number"} min="1"/>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className={style.center}>
+                      <input value={languagePack.search[langProps.language]} type={"submit"}/>
+                    </Col>
+                  </Row>
+                </Container>
+              </form>
+            </Col>
+            <Col xs={12} sm={12} md={3} lg={2} xl={3}/>
+          </Row>
+
+        </Container>
+    )}
+  </LanguageContext.Consumer>
+</>)
 
 export default Search;

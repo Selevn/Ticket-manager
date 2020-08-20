6be286979c46149895b4bbcccd2f6b2888366291
Ivan Skorodumov <53996471+Selevn@users.noreply.github.com>
2020-08-20 15:14:00 +0300

    wrapped in provider

--- src/index.js
+++ src/index.js
@@ -6,11 +6,30 @@ import React from "react";
 
 import mainStore from "./js/store/concerts.store.js"
 
-import {Provider} from "react-redux";
+import {connect, Provider} from "react-redux";
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {actionGetAllConcertsSorted} from "./js/actions/concerts.actions.js";
 
+const mapStateToProps = (state) => {
+  return {concerts: state.concertList}
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    getConcerts: () => {
+      dispatch(actionGetAllConcertsSorted())
+    }
+  }
+}
+const WrappedComponent = connect(mapStateToProps, mapDispatchToProps)(AppContainer)
 
 const wrapper = document.getElementById("root");
-wrapper ? ReactDOM.render(<BrowserRouter><AppContainer
-    getConcerts={getConcerts}/></BrowserRouter>, wrapper) : false;
+wrapper ? ReactDOM.render(
+    <BrowserRouter>
+      <Provider state={mainStore}>
+        <WrappedComponent/>
+      </Provider>
+    </BrowserRouter>, wrapper) : false;
+
+/*<AppContainer
+    getConcerts={getConcerts}/>*/

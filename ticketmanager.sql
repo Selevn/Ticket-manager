8ee765bb13adfe8cecfee81aac61d2ec04c33b33
Selevn <van000200136@gmail.com>
2020-09-01 21:27:02 +0300

    db arhitecture upd

--- ticketmanager.sql
+++ ticketmanager.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Хост: 127.0.0.1
--- Время создания: Авг 31 2020 г., 12:58
--- Версия сервера: 10.4.11-MariaDB
--- Версия PHP: 7.4.3
+-- Host: 127.0.0.1
+-- Generation Time: Sep 01, 2020 at 08:29 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `ticketmanager`
+-- Database: `ticketmanager`
 --
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `concerts`
+-- Table structure for table `concerts`
 --
 
 CREATE TABLE `concerts` (
@@ -37,7 +36,7 @@ CREATE TABLE `concerts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Дамп данных таблицы `concerts`
+-- Dumping data for table `concerts`
 --
 
 INSERT INTO `concerts` (`id`, `band`, `place`, `date`, `img`) VALUES
@@ -52,7 +51,7 @@ INSERT INTO `concerts` (`id`, `band`, `place`, `date`, `img`) VALUES
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `halls`
+-- Table structure for table `halls`
 --
 
 CREATE TABLE `halls` (
@@ -63,7 +62,7 @@ CREATE TABLE `halls` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Дамп данных таблицы `halls`
+-- Dumping data for table `halls`
 --
 
 INSERT INTO `halls` (`id`, `place`, `description`, `img`) VALUES
@@ -73,7 +72,54 @@ INSERT INTO `halls` (`id`, `place`, `description`, `img`) VALUES
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `users`
+-- Table structure for table `sector`
+--
+
+CREATE TABLE `sector` (
+  `place` varchar(40) DEFAULT NULL,
+  `id` int(4) NOT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `numOfSeats` int(4) NOT NULL,
+  `features` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`features`)),
+  `vipFeatures` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`vipFeatures`)),
+  `vipNum` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sector`
+--
+
+INSERT INTO `sector` (`place`, `id`, `name`, `numOfSeats`, `features`, `vipFeatures`, `vipNum`) VALUES
+('Chizhovka', 1, 'Left', 200, '{}', '{}', 0),
+('Chizhovka', 2, 'right', 200, '{}', '{}', 0),
+('Chizhovka', 3, 'center', 1000, '{}', '{}', 100);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `id` int(8) NOT NULL,
+  `userId` int(4) DEFAULT NULL,
+  `place` varchar(40) DEFAULT NULL,
+  `sector` int(6) DEFAULT NULL,
+  `cost` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `ticket`
+--
+
+INSERT INTO `ticket` (`id`, `userId`, `place`, `sector`, `cost`) VALUES
+(1, 12, 'Chizhovka', 2, 199.99),
+(2, 12, 'Chizhovka', 3, 149.99);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -87,70 +133,112 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Дамп данных таблицы `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `name`, `sname`, `password`, `isApproved`, `userType`) VALUES
 (1, 'van000200136@gmail.com', NULL, NULL, '123456', 1, 'admin'),
 (10, 'van000222001362@gmail.com', NULL, NULL, '123456', 0, 'user'),
 (11, 'van00020013123136@gmail.com', NULL, NULL, '1234123412312', 0, '0'),
-(12, 'v@gmail.com', NULL, NULL, '123456', 0, 'user');
+(12, 'v@gmail.com', NULL, NULL, '123456', 1, 'user');
 
 --
--- Индексы сохранённых таблиц
+-- Indexes for dumped tables
 --
 
 --
--- Индексы таблицы `concerts`
+-- Indexes for table `concerts`
 --
 ALTER TABLE `concerts`
   ADD PRIMARY KEY (`id`),
   ADD KEY `place` (`place`);
 
 --
--- Индексы таблицы `halls`
+-- Indexes for table `halls`
 --
 ALTER TABLE `halls`
   ADD PRIMARY KEY (`id`),
   ADD KEY `place` (`place`);
 
 --
--- Индексы таблицы `users`
+-- Indexes for table `sector`
+--
+ALTER TABLE `sector`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `place` (`place`);
+
+--
+-- Indexes for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`),
+  ADD KEY `sector` (`sector`),
+  ADD KEY `ticket_ibfk_2` (`place`);
+
+--
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT для сохранённых таблиц
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT для таблицы `concerts`
+-- AUTO_INCREMENT for table `concerts`
 --
 ALTER TABLE `concerts`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT для таблицы `halls`
+-- AUTO_INCREMENT for table `halls`
 --
 ALTER TABLE `halls`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT для таблицы `users`
+-- AUTO_INCREMENT for table `sector`
+--
+ALTER TABLE `sector`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- Ограничения внешнего ключа сохраненных таблиц
+-- Constraints for dumped tables
 --
 
 --
--- Ограничения внешнего ключа таблицы `concerts`
+-- Constraints for table `concerts`
 --
 ALTER TABLE `concerts`
   ADD CONSTRAINT `concerts_ibfk_1` FOREIGN KEY (`place`) REFERENCES `halls` (`place`);
+
+--
+-- Constraints for table `sector`
+--
+ALTER TABLE `sector`
+  ADD CONSTRAINT `sector_ibfk_1` FOREIGN KEY (`place`) REFERENCES `halls` (`place`);
+
+--
+-- Constraints for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `ticket_ibfk_2` FOREIGN KEY (`place`) REFERENCES `sector` (`place`),
+  ADD CONSTRAINT `ticket_ibfk_3` FOREIGN KEY (`sector`) REFERENCES `sector` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

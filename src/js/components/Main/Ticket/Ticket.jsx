85e2b2165ce43b06e4a154a65c9a54c7133d6adf
Ivan Skorodumov <53996471+Selevn@users.noreply.github.com>
2020-09-09 20:55:58 +0300

    small fixes, bd conflict start resolve

--- src/js/components/Main/Ticket/Ticket.jsx
+++ src/js/components/Main/Ticket/Ticket.jsx
@@ -7,55 +7,76 @@ import {LanguageContext} from "../../Contexts/LanguageContext.js";
 import languageSrc from "../../../language.js";
 
 
-const Ticket = ({data, back, currentSectorDesc, setSectorDesc,buyTicket, count, countChange}) => (
+const Ticket = ({data, back, currentSectorDesc, setSectorDesc, buyTicket, count, countChange}) => (
     <LanguageContext.Consumer>
-        {langProps => {
-            return (<>
-            <Container className={style.mainContainer}>
-                <Row className = {"headerRow"}>
-                    <Col className={style.centre}><h1>{languageSrc.buyTicket[langProps.language]}</h1></Col>
-                </Row>
-                <Row className={style.dataRow}>
-                    <Col className={style.sectorColumn}>
-                        {
-                            data[0] && data.map((item)=>{
-                                return(
-                                    <Container className={style.sectorBlock} onClick={()=>{setSectorDesc(item.id)}}>
-                                        <Row>
-                                            <Col></Col>
-                                            <Col className={style.sectorName}>{item.name}</Col>
-                                            <Col className={style.sectorCost}>{item.cost}$</Col>
-                                        </Row>
-                                    </Container>)
-                            })
-                        }
-                    </Col>
-                    <Col className={style.centre}>
-                        <h2>Features</h2>
-                        {currentSectorDesc && currentSectorDesc.features}
-                        <h2>Vip Features</h2>
-                        {currentSectorDesc && currentSectorDesc.vipFeatures}
+      {langProps => {
+        return (<>
+          <Container className={style.mainContainer}>
+            <Row className={"headerRow"}>
+              <Col className={style.centre}><h1>{languageSrc.buyTicket[langProps.language]}</h1></Col>
+            </Row>
+            <Row className={style.dataRow}>
+              <Col className={style.sectorColumn}>
+                {
+                  data[0] && data.map((item) => {
+                    return (
+                        <Container className={style.sectorBlock} id={item.id + "sector"} onClick={(i) => {
+                          setSectorDesc(item.id);
+                          let collection = document.getElementsByClassName(style.active)
+                          for (let i = 0; i < collection.length; i++) {
+                            collection[i].classList.remove(style.active)
+                          }
+                          document.getElementById(item.id + "sector").classList.toggle(style.active)
+                        }}>
+                          <Row>
+                            <Col></Col>
+                            <Col className={style.sectorName}>{item.name}</Col>
+                            <Col className={style.sectorCost}>{item.cost}$</Col>
+                          </Row>
+                        </Container>)
+                  })
+                }
+              </Col>
+              <Col className={style.centre}>
+                {currentSectorDesc && (<h2>Features</h2>)}
+                {currentSectorDesc && currentSectorDesc.features}
+                <br/>
+                {currentSectorDesc && "Num of seats: "+currentSectorDesc.numOfSeats}
 
-                    </Col>
-                </Row>
-                <Row className={style.centre}>
-                    <Col><button className={style.buyButton} onClick={back}>{languageSrc.back[langProps.language]}</button></Col>
-                    <Col/>
-                    <Col><input type="number" min={1} max = {12} value={count} onChange={countChange} className = {style.counter}></input><button className={style.buyButton} onClick={buyTicket}>{languageSrc.buyTicket[langProps.language]}</button></Col>
-                </Row>
 
-            </Container>
-            </>)
-        }
-        }
+                {currentSectorDesc && (<h2>Vip Features</h2>)}
+                {currentSectorDesc && currentSectorDesc.vipFeatures}
+                <br/>
+                {currentSectorDesc && "Num of VIP seats: "+currentSectorDesc.vipNum}
+
+              </Col>
+            </Row>
+            <Row className={style.centre}>
+              <Col>
+                <button className={style.buyButton} onClick={back}>{languageSrc.back[langProps.language]}</button>
+              </Col>
+              <Col/>
+              <Col><input type="number" min={1} max={12} value={count} onChange={countChange}
+                          className={style.counter}/>
+                <button className={style.buyButton}
+                        onClick={buyTicket}>
+                  {languageSrc.buyTicket[langProps.language]}
+                </button>
+              </Col>
+            </Row>
+
+          </Container>
+        </>)
+      }
+      }
     </LanguageContext.Consumer>
 )
 
 Ticket.propTypes = {
-    concert: PropTypes.object,
-    back: PropTypes.func,
-    data: PropTypes.object,
-    buyTicket: PropTypes.func,
+  concert: PropTypes.object,
+  back: PropTypes.func,
+  data: PropTypes.object,
+  buyTicket: PropTypes.func,
 }
 
 export default Ticket;

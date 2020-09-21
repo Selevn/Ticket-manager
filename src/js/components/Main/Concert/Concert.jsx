bf3e5f2182867556d7c6a1945ef329f8a405830d
Selevn <van000200136@gmail.com>
2020-09-21 18:56:56 +0300

    concert page design

--- src/js/components/Main/Concert/Concert.jsx
+++ src/js/components/Main/Concert/Concert.jsx
@@ -1,84 +1,111 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import PropTypes from "prop-types"
-import {Container, Row, Col} from "react-bootstrap"
 
 import style from "./Concert.module.css"
 import {LanguageContext} from "../../Contexts/LanguageContext.js";
 import languageSrc from "../../../language.js";
 
+import Particles from "react-particles-js";
 
-const Concert = ({concert, back, buy}) => (
+const Concert = ({concert, back, buy}) => {
+  const particle = useMemo(()=>(<Particles
+      params={{
+        particles: {
+          number: {
+            value: 30,
+            density:{
+              enable:true,
+              value_area: 500,
+            },
+          },
+          color: {
+            value: "#000000"
+          },
+          opacity: {
+            value: 0.5,
+            random: true
+          },
+          size:
+            {value: 2},
+          line_linked: {
+            enable:true,
+            distance:150,
+            color:"#000",
+          },
+          move:
+            {
+              speed:1
+            }
+        }
+      }}
+      //TODO: РАСТЯНУТЬ КАНВАС ПО ВЫСОТЕ!
+      canvasClassName={style.backgroundPartickle}
+      width = {"100%"}
+      height = {"100%"}/>
+  ),[])
+  return(
     <LanguageContext.Consumer>
-        {langProps => {
-            if (!concert) {
-                return (<></>)
-            }
-            let day, month, year, hour, minute;
-            if (concert.hasOwnProperty("id")) {
-                day = new Date(concert.date).getDate() + 1;
-                month = languageSrc.months[new Date(concert.date).getMonth()][langProps.language];
-                year = new Date(concert.date).getFullYear();
-                hour = String(new Date(concert.date).getHours()).padStart(2, '0');
-                minute = String(new Date(concert.date).getMinutes()).padStart(2, '0');
+      {langProps => {
+        if (!concert) {
+          return (<></>)
+        }
+        let day, month, year, hour, minute;
+        if (concert.hasOwnProperty("id")) {
+          day = new Date(concert.date).getDate() + 1;
+          month = languageSrc.months[new Date(concert.date).getMonth()][langProps.language];
+          year = new Date(concert.date).getFullYear();
+          hour = String(new Date(concert.date).getHours()).padStart(2, '0');
+          minute = String(new Date(concert.date).getMinutes()).padStart(2, '0');
 
-            }
+        }
+        return (<>
+          {particle}
+          <div className={style.mainContainer}>
 
-            return (<>
-                <div className={style.mainContainer}>
-                    <div className={style.imageBlock}>
-                        <img className={style.titleImage}
-                             src={concert.imgSrc}/>
-                    </div>
-                    <div className={style.dataBlock}>
-                        <div className={style.infoCol}>
-                            <div className={style.infoColData}>
-                                <span className={style.leftFloat}>{languageSrc.band[langProps.language]}:</span> <b
-                                className={style.rightFloat}>{concert.band}</b>
-                                <br/>
-                                <span className={style.leftFloat}>{languageSrc.place[langProps.language]}:</span> <b
-                                className={style.rightFloat}>{concert.place}</b>
-                                <br/>
-                                <span className={style.leftFloat}>{languageSrc.date[langProps.language]}:</span> <b
-                                className={style.rightFloat}>
-                                {`${day || ""} ${month || ""} ${year || ""}`}
-                            </b>
-                                <br/>
-                                <span className={style.leftFloat}>{languageSrc.time[langProps.language]}:</span> <b
-                                className={style.rightFloat}>
-                                {`${hour || ""}:${minute || ""}`}
-                            </b>
-                                <br/>
-                            </div>
-                        </div>
-                        <div className={style.buttonPlacement}>
-                                <button className={style.backButton}
-                                        onClick={back}>{languageSrc.back[langProps.language]}</button>
-                                <button className={style.buyButton}
-                                        onClick={buy}>{languageSrc.ticket[langProps.language]}</button>
-                        </div>
-                    </div>
-                    {/*<div className={style.buttonPlacement}>
-
-
-              <div className={style.buttonColumn}>
-                <button className={style.backButton} onClick={back}>{languageSrc.back[langProps.language]}</button>
-              </div>
-              <div>
-                <button className={style.buyButton} onClick={buy}>{languageSrc.ticket[langProps.language]}</button>
-              </div>
-            </div>*/}
+            <div className={style.imageBlock}>
+              <img className={style.titleImage}
+                   src={concert.imgSrc}/>
+            </div>
+            <div className={style.dataBlock}>
+              <div className={style.infoCol}>
+                <div className={style.infoColData}>
+                  <span className={style.leftFloat}>{languageSrc.band[langProps.language]}:</span> <b
+                  className={style.rightFloat}>{concert.band}</b>
+                  <br/>
+                  <span className={style.leftFloat}>{languageSrc.place[langProps.language]}:</span> <b
+                  className={style.rightFloat}>{concert.place}</b>
+                  <br/>
+                  <span className={style.leftFloat}>{languageSrc.date[langProps.language]}:</span> <b
+                  className={style.rightFloat}>
+                  {`${day || ""} ${month || ""} ${year || ""}`}
+                </b>
+                  <br/>
+                  <span className={style.leftFloat}>{languageSrc.time[langProps.language]}:</span> <b
+                  className={style.rightFloat}>
+                  {`${hour || ""}:${minute || ""}`}
+                </b>
+                  <br/>
                 </div>
-
-            </>)
-        }
-        }
+              </div>
+              <div className={style.buttonPlacement}>
+                <button className={style.backButton}
+                        onClick={back}>{languageSrc.back[langProps.language]}</button>
+                <button className={style.buyButton}
+                        onClick={buy}>{languageSrc.ticket[langProps.language]}</button>
+              </div>
+            </div>
+          </div>
+        </>)
+      }
+      }
     </LanguageContext.Consumer>
-)
+  )
+}
 
 Concert.propTypes = {
-    concert: PropTypes.object,
-    back: PropTypes.func,
-    buy: PropTypes.func,
+  concert: PropTypes.object,
+  back: PropTypes.func,
+  buy: PropTypes.func,
 }
 
 export default Concert;
--- src/js/components/Main/Schedule/Schedule.jsx
+++ src/js/components/Main/Schedule/Schedule.jsx
@@ -9,185 +9,185 @@ import {Link} from "react-router-dom";
 import Particles from "react-particles-js";
 
 const Schedule = ({concerts}) => {
-    const particle = useMemo(()=>(<Particles
-            params={{
-                particles: {
-                    number: {
-                        value: 30,
-                        density:{
-                            enable:true,
-                            value_area: 500,
-                        },
-                    },
-                    color: {
-                        value: "#000000"
-                    },
-                    opacity: {
-                        value: 0.5,
-                        random: true
-                    },
-                    size:
-                        {value: 2},
-                    line_linked: {
-                        enable:true,
-                        distance:150,
-                        color:"#000",
-                    },
-                    move:
-                        {
-                            speed:1
-                        }
-                }
-            }}
-            //TODO: РАСТЯНУТЬ КАНВАС ПО ВЫСОТЕ!
-            canvasClassName={style.backgroundPartickle}
-            width = {"100%%"}
-            height = {"100%"}/>
-    ),[])
-    return (
-        <LanguageContext.Consumer>
-            {langProps => {
-                let data = [];
-                concerts = concerts.map(item => ({...item, date: new Date(item.date)})).map(item => {
-                        let season = (item) => {
-                            switch (item.date.getMonth()) {
-                                case 0:
-                                    return "winter"
-                                case 1:
-                                    return "winter"
-                                case 2:
-                                    return "spring"
-                                case 3:
-                                    return "spring"
-                                case 4:
-                                    return "spring"
-                                case 5:
-                                    return "summer"
-                                case 6:
-                                    return "summer"
-                                case 7:
-                                    return "summer"
-                                case 8:
-                                    return "autumn"
-                                case 9:
-                                    return "autumn"
-                                case 10:
-                                    return "autumn"
-                                case 11:
-                                    return "winter"
-                            }
-                        }
-                        return {...item, season: season(item)}
-                    })
-                for (let i = 0; i < concerts.length; i++) {
-                    if (data.indexOf(concerts[i].date.getFullYear()) === -1) {
-                        data.push(concerts[i].date.getFullYear())
-                        data.push({type: "month", data: concerts[i].date.getMonth(), arr: []})
-                    }
-                    if (concerts[i].date.getMonth() === data[data.length - 1].data) {
-                        data[data.length - 1].arr.push(concerts[i])
-                    } else {
-                        data.push({type: "month", data: concerts[i].date.getMonth(), arr: []})
-                        data[data.length - 1].arr.push(concerts[i])
-                    }
-                }
-
-                const ConcertItem = ({item}) => {
-                    let date = new Date(item.date);
-                    return (
-                        <Link to={"concert/" + item.id} className={style.cardLink}>
-                            <div className={style.concertItem}>
-
-                                <div className={style.cardImageDiv}>
-                                    <img src={item.imgSrc} className={style.cardImage}/>
-                                </div>
-                                <div className={style.cardInfoDiv}>
-                                    <div className={style.concertItemBand}> {item.band}</div>
-                                    <div className={style.concertItemPlace}> {item.place}
-                                    </div>
-                                </div>
-                                <div
-                                    className={style.cardFooter}>{languageSrc.months[date.getMonth()][langProps.language] + " " + date.getDate()}</div>
-
-
-                            </div>
-                        </Link>
-                    )
-                }
-                const MonthRow = ({concerts}) => {
-                    if (concerts.length !== 0) {
-                        let season = concerts[0].season;
-                        if (season == "autumn" || season == "spring")
-                            return (
-
-                                <div className={style.monthContainer + " " + style[season + "Data"]}>
-                                    {particle}
-                                <div className={style.monthContainerName + " " + style[season + "Name"]}>
-                                    <span>{languageSrc.months[concerts[0].date.getMonth()][langProps.language]}</span>
-                                </div>
-                                <div className={style.monthContainerData}>
-                                    {
-                                        concerts.map((item, index) =>
-                                            (<ConcertItem item={item} key={index}/>)
-                                        )
-                                    }
-                                </div>
-                            </div>)
-                        else
-                            return (
-                                <div className={style.monthContainer + " " + style[season + "Data"]}>
-                                    {particle}
-                                    <div className={style.monthContainerData}>
-                                        {
-                                            concerts.map((item, index) =>
-                                                (<ConcertItem item={item} key={index}/>)
-                                            )
-                                        }
-                                    </div>
-                                    <div className={style.monthContainerName + " " + style[season + "Name"]}>
-                                        <span>{languageSrc.months[concerts[0].date.getMonth()][langProps.language]}</span>
-                                    </div>
-                                </div>)
-                    } else
-                        return (<></>)
-                }
-                const YearItem = (year) => {
-                    return (
-                        <div className={style.year}>
-                            {year.year}
-                        </div>
-                    )
-                }
-
-                return (
-                    <>
-
-                    <div className={"mainContainer"}>
-                        {particle}
-                        <div className={"pageHeaderRow " + style.pageHeaderSchedule}>
-                            <div className={"pageHeader"}>
-                                {languageSrc.schedule[langProps.language]}
-                            </div>
-                        </div>
-                        <div className={style.dataRow}>
-                            {
-                                data.map((item) => {
-                                    if (typeof (item) === "number")
-                                        return (<YearItem year={item}/>)
-                                    else
-                                        return (<MonthRow concerts={item.arr}/>)
-                                })
-
-                            }
-                        </div>
-                    </div>
-
-                    </>
-                )
+  const particle = useMemo(() => (<Particles
+      params={{
+        particles: {
+          number: {
+            value: 30,
+            density: {
+              enable: true,
+              value_area: 500,
+            },
+          },
+          color: {
+            value: "#000000"
+          },
+          opacity: {
+            value: 0.5,
+            random: true
+          },
+          size:
+            {value: 2},
+          line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#000",
+          },
+          move:
+            {
+              speed: 1
             }
+        }
+      }}
+      //TODO: РАСТЯНУТЬ КАНВАС ПО ВЫСОТЕ!
+      canvasClassName={style.backgroundPartickle}
+      width={"100%%"}
+      height={"100%"}/>
+  ), [])
+  return (
+    <LanguageContext.Consumer>
+      {langProps => {
+        let data = [];
+        concerts = concerts.map(item => ({...item, date: new Date(item.date)})).map(item => {
+          let season = (item) => {
+            switch (item.date.getMonth()) {
+              case 0:
+                return "winter"
+              case 1:
+                return "winter"
+              case 2:
+                return "spring"
+              case 3:
+                return "spring"
+              case 4:
+                return "spring"
+              case 5:
+                return "summer"
+              case 6:
+                return "summer"
+              case 7:
+                return "summer"
+              case 8:
+                return "autumn"
+              case 9:
+                return "autumn"
+              case 10:
+                return "autumn"
+              case 11:
+                return "winter"
             }
-        </LanguageContext.Consumer>
-    )
+          }
+          return {...item, season: season(item)}
+        })
+        for (let i = 0; i < concerts.length; i++) {
+          if (data.indexOf(concerts[i].date.getFullYear()) === -1) {
+            data.push(concerts[i].date.getFullYear())
+            data.push({type: "month", data: concerts[i].date.getMonth(), arr: []})
+          }
+          if (concerts[i].date.getMonth() === data[data.length - 1].data) {
+            data[data.length - 1].arr.push(concerts[i])
+          } else {
+            data.push({type: "month", data: concerts[i].date.getMonth(), arr: []})
+            data[data.length - 1].arr.push(concerts[i])
+          }
+        }
+
+        const ConcertItem = ({item}) => {
+          let date = new Date(item.date);
+          return (
+            <Link to={"concert/" + item.id} className={style.cardLink}>
+              <div className={style.concertItem}>
+
+                <div className={style.cardImageDiv}>
+                  <img src={item.imgSrc} className={style.cardImage}/>
+                </div>
+                <div className={style.cardInfoDiv}>
+                  <div className={style.concertItemBand}> {item.band}</div>
+                  <div className={style.concertItemPlace}> {item.place}
+                  </div>
+                </div>
+                <div
+                  className={style.cardFooter}>{languageSrc.months[date.getMonth()][langProps.language] + " " + date.getDate()}</div>
+
+
+              </div>
+            </Link>
+          )
+        }
+        const MonthRow = ({concerts}) => {
+          if (concerts.length !== 0) {
+            let season = concerts[0].season;
+            if (season == "autumn" || season == "spring")
+              return (
+
+                <div className={style.monthContainer + " " + style[season + "Data"]}>
+                  {particle}
+                  <div className={style.monthContainerName + " " + style[season + "Name"]}>
+                    <span>{languageSrc.months[concerts[0].date.getMonth()][langProps.language]}</span>
+                  </div>
+                  <div className={style.monthContainerData}>
+                    {
+                      concerts.map((item, index) =>
+                        (<ConcertItem item={item} key={index}/>)
+                      )
+                    }
+                  </div>
+                </div>)
+            else
+              return (
+                <div className={style.monthContainer + " " + style[season + "Data"]}>
+                  {particle}
+                  <div className={style.monthContainerData}>
+                    {
+                      concerts.map((item, index) =>
+                        (<ConcertItem item={item} key={index}/>)
+                      )
+                    }
+                  </div>
+                  <div className={style.monthContainerName + " " + style[season + "Name"]}>
+                    <span>{languageSrc.months[concerts[0].date.getMonth()][langProps.language]}</span>
+                  </div>
+                </div>)
+          } else
+            return (<></>)
+        }
+        const YearItem = (year) => {
+          return (
+            <div className={style.year}>
+              {year.year}
+            </div>
+          )
+        }
+
+        return (
+          <>
+
+            <div className={"mainContainer"}>
+              {particle}
+              <div className={"pageHeaderRow " + style.pageHeaderSchedule}>
+                <div className={"pageHeader"}>
+                  {languageSrc.schedule[langProps.language]}
+                </div>
+              </div>
+              <div className={style.dataRow}>
+                {
+                  data.map((item) => {
+                    if (typeof (item) === "number")
+                      return (<YearItem year={item}/>)
+                    else
+                      return (<MonthRow concerts={item.arr}/>)
+                  })
+
+                }
+              </div>
+            </div>
+
+          </>
+        )
+      }
+      }
+    </LanguageContext.Consumer>
+  )
 }
 
 /*<div className={style.yearData}>
@@ -233,8 +233,8 @@ const Schedule = ({concerts}) => {
                             </div>
 */
 Schedule.propTypes = {
-    concerts: PropTypes.array,
-    getConcerts: PropTypes.func,
+  concerts: PropTypes.array,
+  getConcerts: PropTypes.func,
 }
 
 export default Schedule;
